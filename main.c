ccd2b04beec1fa5e1aac86201f2e3aabe2ac7a2d
EtherNeil <neil.tessier01@gmail.com>
2024-11-24 21:33:43 +0100

    Add .gitignore and implement data types and array functions in main.c

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!");
+int dataTypes()
+{
+    int a = 3;
+    float b = 4.5;
+    double c = 5.25;
+    float sum;
+    sum = a + b + c;
+    printf("The sum of a, b and c is %f.\n", sum);
+    return 0;
+}
+
+int array()
+{
+    int average;
+    int grades[10];
+    grades[0] = 80;
+    grades[1] = 81;
+    grades[2] = 89;
+    grades[3] = 90;
+    average = (grades[0] + grades[1] + grades[2] + grades[3]) / 4;
+    printf("The average of the 4 grades is: %d\n", average);
+    return 0;
+}
+
+int main()
+{
+    printf("Hello, World!\n");
+    dataTypes();
+    array();
     return 0;
 }
